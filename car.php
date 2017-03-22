b821d0f3ff2c7205b01077c2ecb0eac8d4db461d
Anjali vijay <anjali.vijay@qed42.com>
2017-03-22 19:44:59 +0530

    after changes

--- car.php
+++ car.php
@@ -6,18 +6,18 @@ class Car{
     public $brand;
 
     public function __construct(){
-       echo 'About this Car.<br />';
+       echo 'About this Car.';
     }
 
     public function showColor(){
-       echo 'car color $'.$this->color.'.<br />';
+       echo 'car color'.$this->color.'.';
     }
 
     public function showMileage(){
-       echo 'This car has '.$this->mileage.'.<br />';
+       echo 'This car has '.$this->mileage.'.';
     }
     public function showBrand(){
-       echo 'This car is of '.$this->brand.'.<br />';
+       echo 'This car is of '.$this->brand.'.';
     }
 }
 ?>
--- display.php
+++ display.php
@@ -2,9 +2,26 @@
 include('car.php');
 
 class XUV extends Car{
-    private $color;
-    private $mileage;
-    private $brand;
+    public $color;
+    public $mileage;
+    public $brand;
+    public function setColor($color){
+      $this->color=$color;
+    }
+
+    public function setMileage($mileage){
+      $this->mileage = $Mileage;
+    }
+
+    public function setBrand($brand){
+      $this->brand = $Brand;
+    }
+}
+
+class Sedan extends Car{
+    public $color;
+    public $mileage;
+    public $brand;
     public function setColor($color){
       $this->color=$color;
     }
@@ -22,7 +39,13 @@ $carObj = new XUV;
 $carObj->color = 'red';
 $carObj->mileage = 20;
 $carObj->brand = 'xuv';
+echo $carObj->color.': '.$carObj->mileage.':'.$carObj->brand.'<br/>';
+
+$carObj = new Sedan;
+$carObj->color = 'blue';
+$carObj->mileage = 30;
+$carObj->brand = 'sedan';
 
 
-echo $carObj->color.': '.$carObj->mileage.':'.$carObj->brand.'<br />';
+echo $carObj->color.': '.$carObj->mileage.':'.$carObj->brand.'<br/>';
 ?>
